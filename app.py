3fefdf86dcb0a1d5b09282e1e5cad3714c483687
unkonown <amachi057@gmail.com>
2020-06-05 05:55:23 +0900

    a

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app=Flask(__name__)
 @app.route("/")
 def index():
         return "Hello World! ハローワールド!"
-app.run(port=int(os.environ.get('PORT', 8000)))
+app.run(host='0.0.0.0',port=int(os.environ.get('PORT', 5000)))
 
