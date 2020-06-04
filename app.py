3ac78873b1a9e1ea76a0a244deb9c354c4641046
unkonown <amachi057@gmail.com>
2020-06-05 05:51:09 +0900

    a

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, abort, make_response
+import os
 app=Flask(__name__)
 @app.route("/")
 def index():
         return "Hello World! ハローワールド!"
-app.run(port=3000)
+app.run(port=int(os.environ.get('PORT', 8000)))
+
