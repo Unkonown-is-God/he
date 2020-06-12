a7e3be73906216d0e2469483b4ace6e3c0062db1
unkonown <amachi057@gmail.com>
2020-06-13 00:50:13 +0900

    a

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ import craft
 app=Flask(__name__)
 @app.route("/<key>",methods=["GET"])
 def main(key):
-        return craft.apimode(key)
+        return jsonify(craft.apimode(key))
 
 app.run(host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
