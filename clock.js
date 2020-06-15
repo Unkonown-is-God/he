52b4de978a08fb774ce946716275e6355b9b06ff
unkonown <amachi057@gmail.com>
2020-06-15 16:45:58 +0900

    a

--- clock.js
+++ clock.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const URL = 'http://your-herokuapp-apitest010.herokuapp.com'; // 独自ドメインでもOK
+const URL = 'https://apitest010.herokuapp.com'; // 独自ドメインでもOK
 const INTERVAL_MSEC = 10 * 60 * 1000; // 10分毎(30分未満ならOK)
 
 setInterval(() => {
