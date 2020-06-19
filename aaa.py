a35461d5696ed42ea0b6e880b0c784fa8fbff227
unkonown <amachi057@gmail.com>
2020-06-19 20:14:52 +0900

    emotion

--- aaa.py
+++ aaa.py
@@ -1,5 +1,5 @@
-from janome.tokenizer import Tokenizer
-
-t = Tokenizer()
-for token in t.tokenize(u'おはよう'):
-  print(token)
+with open ('dics/emotion.txt','r',encoding='utf-8') as f:
+  lines=[x for x in f.read().splitlines() if x]#リスト内表記で検索
+  l=len(lines)
+  parts=[lines[x].split(':') for x in range(l)]
+  print(parts)
