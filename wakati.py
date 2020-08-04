af35e3c4bcafde85b87e2eab916fb0126b9bc6c1
Unkonown-is-God <63834985+Unkonown-is-God@users.noreply.github.com>
2020-08-04 15:27:11 +0900

    encodeing

--- wakati.py
+++ wakati.py
@@ -7,9 +7,17 @@ def wakati_text(text):
     t = Tokenizer()
     return t.tokenize(text,wakati=True)
 
-def add_wakati(sentence):
-    with open(WAKATI,'a') as f:
-        print('\n'.join(sentence),file=f)
+def add_wakati(wakati):
+    with open(WAKATI,'a',encoding='utf-8') as f:
+        print('\n'.join(wakati),file=f)
 
 def load_file(path):
+    #実装途中
     pass
+if __name__ == '__main__': 
+    path=input()
+    sentences=load_file(path)
+    for line in sentences:
+        wakati=wakati_text(line)
+        add_wakati(wakati)
+        
