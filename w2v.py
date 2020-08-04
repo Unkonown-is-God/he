fd4a1d513d1ab7fe1b9e1dcff430477be1b4e724
Unkonown-is-God <63834985+Unkonown-is-God@users.noreply.github.com>
2020-08-04 15:37:07 +0900

    coment

--- w2v.py
+++ w2v.py
@@ -4,7 +4,7 @@ model_file = 'dics/w2v_model.model'
 def load_w2v(word): 
     model = word2vec.Word2Vec.load(model_file)
     try:
-        print(1)
+        #print('w2v')
         similar_words = model.most_similar(positive=[word])
         return random.choice([w[0] for w in similar_words])
     except:
